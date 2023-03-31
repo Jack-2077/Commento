db75efe394e347e577340a7419ebadf218c747ca
Jack <78120697+Jack-2077@users.noreply.github.com>
2023-03-30 21:42:11 -0700

    feat: sort options for comments section based on various criteria

--- src/js/content.js
+++ src/js/content.js
@@ -6,3 +6,39 @@ const commentList = commentsSection.querySelector('#contents');
 
 // Get the sort menu element
 const sortMenu = commentsSection.querySelector('#sort-menu');
+
+// Create the sort options
+const sortOptions = [
+  {
+    name: 'Newest',
+    fn: (a, b) =>
+      new Date(
+        b.querySelector('#header #published-time').getAttribute('datetime')
+      ) -
+      new Date(
+        a.querySelector('#header #published-time').getAttribute('datetime')
+      ),
+  },
+  {
+    name: 'Oldest',
+    fn: (a, b) =>
+      new Date(
+        a.querySelector('#header #published-time').getAttribute('datetime')
+      ) -
+      new Date(
+        b.querySelector('#header #published-time').getAttribute('datetime')
+      ),
+  },
+  {
+    name: 'Top Comments',
+    fn: (a, b) =>
+      parseFloat(b.querySelector('#header #vote-count-middle').innerText) -
+      parseFloat(a.querySelector('#header #vote-count-middle').innerText),
+  },
+  {
+    name: 'Worst Comments',
+    fn: (a, b) =>
+      parseFloat(a.querySelector('#header #vote-count-middle').innerText) -
+      parseFloat(b.querySelector('#header #vote-count-middle').innerText),
+  },
+];
