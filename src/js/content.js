407bd9ee3b5b8be0ea3d3fc5cb42802ade26a476
Jack <78120697+Jack-2077@users.noreply.github.com>
2023-03-30 21:39:53 -0700

    feat: retrieve comments section, comment list, and sort menu elements

---
+++ src/js/content.js
@@ -0,0 +1,8 @@
+// Get the comments section element
+const commentsSection = document.querySelector('#comments');
+
+// Get the comment list element
+const commentList = commentsSection.querySelector('#contents');
+
+// Get the sort menu element
+const sortMenu = commentsSection.querySelector('#sort-menu');
